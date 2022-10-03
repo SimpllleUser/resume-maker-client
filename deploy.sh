058587dd7c947313d0cc6d6d084a4acda1211763
SimpllleUser <pecheritsa.vv@gmail.com>
2022-10-03 14:33:50 +0300

    update deploy config

--- deploy.sh
+++ deploy.sh
@@ -20,4 +20,4 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:SimpllleUser/resume-maker-client.git master:gh-pages
+git push -f git@github.com:SimpllleUser/resume-maker-client.git main:gh-pages
