608210287a4813343a2c08e501c0980e4fce88b2
simpllleser <pecherytsia.vv@gmail.com>
2023-03-17 00:08:31 +0200

    feat: add style to title container

--- src/components/MainForm.vue
+++ src/components/MainForm.vue
@@ -1,14 +1,27 @@
 <script setup lang="ts">
 import ResumeMainInfo from './Resume/ResumeMainInfo.vue';
 import ResumeSkills from './Resume/ResumeSkills.vue';
+import FocusContainer from './FocusContainer.vue';
+import InputTag from './Input/InputTag.vue';
+import { ref } from 'vue';
 // import ResumeCalendar from './Resume/ResumeCalendar.vue';
 // import ResumePhoto from './Resume/ResumePhoto.vue';
+const skillTitle = ref('Skill')
 
 </script>
 <template>
     <div class="max-w-[990px] mx-auto border border-solid border-gray-300">
         <resume-main-info />
-        <resume-skills />
+        <focus-container>
+            <template #header>
+                <div class="flex justify-center items-center py-6 container-title-line">
+                <div class="bg-white px-6">
+                    <input-tag v-model="skillTitle" style="min-width: 10rem;" class="text-center text-2xl font-semibold bg-white" />
+                </div>
+                </div>
+            </template>
+            <resume-skills />
+        </focus-container>
         <!-- <resume-calendar /> -->
     </div>
 </template>
