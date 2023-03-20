86208430d57850c6dc5cb048dbafef919c38bd2a
simpllleser <pecherytsia.vv@gmail.com>
2023-03-20 22:51:35 +0200

    feat(ResumeAbout): base

--- src/components/MainForm.vue
+++ src/components/MainForm.vue
@@ -3,10 +3,12 @@ import ResumeMainInfo from './Resume/ResumeMainInfo.vue';
 import ResumeSkills from './Resume/ResumeSkills.vue';
 import FocusContainer from './FocusContainer.vue';
 import InputTag from './Input/InputTag.vue';
+import ResumeAbout from './Resume/ResumeAbout.vue';
 import { ref } from 'vue';
 // import ResumeCalendar from './Resume/ResumeCalendar.vue';
 // import ResumePhoto from './Resume/ResumePhoto.vue';
-const skillTitle = ref('Skill')
+const skillTitle = ref('Skill');
+const aboutTitle = ref('About');
 
 </script>
 <template>
@@ -22,7 +24,17 @@ const skillTitle = ref('Skill')
             </template>
             <resume-skills />
         </focus-container>
-        <!-- <resume-calendar /> -->
+        <resume-main-info />
+        <focus-container>
+            <template #header>
+                <div class="flex justify-center items-center py-6 container-title-line">
+                <div class="bg-white px-6">
+                    <input-tag v-model="aboutTitle" class="container-title-input" />
+                </div>
+                </div>
+            </template>
+            <resume-about />
+        </focus-container>
     </div>
 </template>
 
---
+++ src/components/Resume/ResumeAbout.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { Ref, ref } from 'vue';
+import InputTag from '../Input/InputTag.vue';
+
+const testText = `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+      when an unknown printer took a galley of type and scrambled it to make 
+      a type specimen book. It has survived not only five centuries,
+       but also the leap into electronic typesetting, remaining essentially unchanged.
+        It was popularised in the 1960s with the release of Letraset sheets containing 
+        Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
+         PageMaker including versions of Lorem Ipsum`;
+const about: Ref<string> = ref(testText)
+const description: Ref<string> = ref('Description')
+</script>
+
+<template>
+    <div class="flex justify-around items-start">
+        <div>
+            <input-tag v-model="description" />
+        </div>
+        <div>
+            <input-tag v-model="about" />
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
