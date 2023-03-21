f40bc66ce390fa8ba663f10726ebf416c04ee0fd
simpllleser <pecherytsia.vv@gmail.com>
2023-03-22 00:39:16 +0200

    feat: add ResumeExperiance

--- src/components/MainForm.vue
+++ src/components/MainForm.vue
@@ -5,12 +5,14 @@ import FocusContainer from './FocusContainer.vue';
 import InputTag from './Input/InputTag.vue';
 import ResumeAbout from './Resume/ResumeAbout.vue';
 import ResumeEducation from './Resume/ResumeEducation.vue';
+import ResumeExperiance from './Resume/ResumeExperiance.vue';
 import { ref } from 'vue';
 // import ResumeCalendar from './Resume/ResumeCalendar.vue';
 // import ResumePhoto from './Resume/ResumePhoto.vue';
 const skillTitle = ref('Skill');
 const aboutTitle = ref('About');
 const educationTitle = ref('Education');
+const experianceTitle = ref('Experiance');
 
 </script>
 <template>
@@ -26,7 +28,6 @@ const educationTitle = ref('Education');
             </template>
             <resume-skills />
         </focus-container>
-        <resume-main-info />
         <focus-container>
             <template #header>
                 <div class="flex justify-center items-center py-6 container-title-line">
@@ -46,6 +47,16 @@ const educationTitle = ref('Education');
                 </div>
             </template>
             <resume-education />
+        </focus-container>
+        <focus-container>
+            <template #header>
+                <div class="flex justify-center items-center py-6 container-title-line">
+                    <div class="bg-white px-6">
+                        <input-tag v-model="experianceTitle" class="container-title-input" />
+                    </div>
+                </div>
+            </template>
+            <resume-experiance />
         </focus-container>
     </div>
 </template>
---
+++ src/components/Resume/ResumeExperiance.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { ref, Ref } from 'vue';
+import { YearMonthRange } from '../../common/types';
+import InputTag from '../Input/InputTag.vue';
+import YearMonthInputRange from '../Input/YearMonthInputRange.vue';
+import BTemplate from '../UI/BTemplate.vue';
+
+const testText = `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+      when an unknown printer took a galley of type and scrambled it to make 
+      a type specimen book. It has survived not only five centuries,
+       but also the leap into electronic typesetting, remaining essentially unchanged.
+        It was popularised in the 1960s with the release of Letraset sheets containing 
+        Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
+         PageMaker including versions of Lorem Ipsum`;
+const description: Ref<string> = ref(testText);
+const place: Ref<string> = ref('Google');
+const position: Ref<string> = ref('Front end Team Lead');
+const date: Ref<YearMonthRange> = ref({
+    from: {
+        month: '',
+        year: '',
+    },
+    to: {
+        month: '',
+        year: '',
+    },
+});
+</script>
+
+<template>
+    <b-template>
+        <template #details>
+            <input-tag v-model="place" />
+            <input-tag v-model="position" />
+            <year-month-input-range v-model="date"/>
+        </template>
+        <template #description>
+            <input-tag v-model="description" />
+        </template>
+    </b-template>
+</template>
+
+<style scoped></style>
