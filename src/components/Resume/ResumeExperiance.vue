4d34e7e7679ec271c61383618469849d718bb8ca
simpllleser <pecherytsia.vv@gmail.com>
2023-03-23 01:11:51 +0200

    feat: add component for create multiple and editable blocks with inputs

--- src/components/Resume/ResumeExperiance.vue
+++ src/components/Resume/ResumeExperiance.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import { ref, Ref } from 'vue';
-import { YearMonthRange } from '../../common/types';
-import InputTag from '../Input/InputTag.vue';
-import YearMonthInputRange from '../Input/YearMonthInputRange.vue';
-import BTemplate from '../UI/BTemplate.vue';
+import { ref, Ref } from "vue";
+import { YearMonthRange } from "../../common/types";
+import InputTag from "../Input/InputTag.vue";
+import YearMonthInputRange from "../Input/YearMonthInputRange.vue";
+import BTemplate from "../UI/BTemplate.vue";
+import BDuplicator from "../UI/BDuplicator.vue";
+import FocusContainer from "../FocusContainer.vue";
 
 const testText = `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
@@ -13,32 +15,86 @@ const testText = `Lorem Ipsum is simply dummy text of the printing and typesetti
         It was popularised in the 1960s with the release of Letraset sheets containing 
         Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
          PageMaker including versions of Lorem Ipsum`;
+
+const mockDate = {
+  from: {
+    month: "",
+    year: "",
+  },
+  to: {
+    month: "",
+    year: "",
+  },
+};
 const description: Ref<string> = ref(testText);
-const place: Ref<string> = ref('Google');
-const position: Ref<string> = ref('Front end Team Lead');
+const place: Ref<string> = ref("Google");
+const position: Ref<string> = ref("Front end Team Lead");
 const date: Ref<YearMonthRange> = ref({
-    from: {
-        month: '',
-        year: '',
-    },
-    to: {
-        month: '',
-        year: '',
-    },
+  from: {
+    month: "",
+    year: "",
+  },
+  to: {
+    month: "",
+    year: "",
+  },
 });
+
+const mockExperiance = {
+    position: "Front end Team Lead",
+    place: 'google',
+    date: mockDate,
+    description: testText,
+  };
+
+const experiances = ref(
+    [
+  {
+    position: "Front end Team Lead",
+    place: 'google',
+    date: mockDate,
+    description: testText,
+  },
+  {
+    position: "Front end Team Lead 2222",
+    place: 'google 222',
+    date: mockDate,
+    description: '!!!!' + testText,
+  },
+]
+);
+const handleAdd = () => {
+    experiances.value.push(mockExperiance);
+};
+
+const handleRemove = (index: number) => {
+    experiances.value.splice(index, 1);
+};
 </script>
 
 <template>
-    <b-template>
-        <template #details>
-            <input-tag v-model="place" />
-            <input-tag v-model="position" />
-            <year-month-input-range v-model="date"/>
-        </template>
-        <template #description>
-            <input-tag v-model="description" />
-        </template>
-    </b-template>
+  <focus-container #default="{ focus }">
+    <b-duplicator 
+    :properties="experiances"
+     :allow-editable="focus"
+     @add="handleAdd"
+     @remove="handleRemove"
+     #default="{ index }"
+     >
+      <div class="my-2">
+        <b-template>
+          <template #details>
+            <input-tag v-model="experiances[index].place" />
+            <input-tag v-model="experiances[index].position" />
+            <year-month-input-range v-model="experiances[index].date" />
+          </template>
+          <template #description>
+            <input-tag v-model="experiances[index].description" />
+          </template>
+        </b-template>
+      </div>
+    </b-duplicator>
+  </focus-container>
 </template>
 
 <style scoped></style>
---
+++ src/components/UI/BDuplicator.vue
@@ -0,0 +1,53 @@
+import { computed } from "vue";
+
+<script setup lang="ts">
+import { computed, Ref, ref } from "vue";
+
+interface Props {
+  properties: Array<unknown>;
+  allowEditable?: boolean;
+}
+
+const emit = defineEmits<{
+  (event: "add"): void;
+  (event: "remove", payload: number): void;
+}>();
+
+const props = withDefaults(defineProps<Props>(), {
+  allowEditable: false,
+});
+
+const editabelProperty: Ref<Array<unknown>> = ref(props.properties);
+
+const handleAdd = (): void => {
+  emit("add");
+};
+
+const canRemove = computed(() => editabelProperty.value?.length > 1);
+
+const handleRemove = (index: number): void => {
+  emit("remove", index);
+};
+</script>
+
+<template>
+  <div class="relative">
+    <div class="absolute duplicator-action-position" v-show="allowEditable">
+      <button class="btn btn-primary btn-xs" @click="handleAdd()">Add</button>
+    </div>
+    <div
+      v-for="(_, index) in editabelProperty"
+      :key="`property-key-${index}`"
+      class="relative"
+    >
+      <div class="absolute right-0" v-show="allowEditable && canRemove">
+        <button class="btn btn-primary btn-xs" @click="handleRemove(index)">
+          Remove
+        </button>
+      </div>
+      <slot :index="index"></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
