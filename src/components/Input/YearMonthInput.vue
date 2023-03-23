f595143720084f4ec9653a9e17938198920eb148
simpllleser <pecherytsia.vv@gmail.com>
2023-03-24 01:54:36 +0200

    refactor(year-month): change imports, add types, and chnage watch handler method

--- src/components/Input/YearMonthInput.vue
+++ src/components/Input/YearMonthInput.vue
@@ -1,78 +1,92 @@
 <script setup lang="ts">
-import { computed, ref, watch } from 'vue';
-import { useVModel } from '@vueuse/core';
-import { isEqual } from 'lodash';
+import { computed, ComputedRef, ref, watch } from "vue";
+import { useVModel } from "@vueuse/core";
+import { isEqual } from "lodash";
 
-import YearInput from './YearInput.vue';
-import MonthInput from './MonthInput.vue';
+import YearInput from "@/components/input/YearInput.vue";
+import MonthInput from "@/components/input/MonthInput.vue";
 
-type YearOrMonth = 'year' | 'month';
+type YearOrMonth = "year" | "month";
 
 interface ModelValue {
-    year?: string;
-    month?: string;
+  year?: string;
+  month?: string;
 }
 
 interface Props {
-    modelValue: ModelValue;
-    present?: boolean;
-    startFromEnd?: boolean;
-    disable?: YearOrMonth | '';
+  modelValue: ModelValue;
+  present?: boolean;
+  startFromEnd?: boolean;
+  disable?: YearOrMonth | "";
+}
+
+interface Emits {
+  (event: "update:modelValue", payload: ModelValue): void;
 }
 
 const props = withDefaults(defineProps<Props>(), {
-    present: false,
-    startFromEnd: false,
-    disable: '',
+  present: false,
+  startFromEnd: false,
+  disable: "",
 });
-const emit = defineEmits<{
-    (event: 'update:modelValue', payload: ModelValue): void
-}>()
-const data = useVModel(props, 'modelValue', emit)
 
+const emit = defineEmits<Emits>();
+const data = useVModel(props, "modelValue", emit);
 
-const ACTUAL_DATE = new Date();
-const ACTUAL_DATE_YEAR = new Date().getFullYear();
+const ACTUAL_DATE: Date = new Date();
+const ACTUAL_DATE_YEAR: number = new Date().getFullYear();
 
-const getMonthShortNameByIndex = (index: number) => new Date(2000, index)
-    .toLocaleString('en-us', { month: 'short' });
+const getMonthShortNameByIndex = (index: number): string =>
+  new Date(2000, index).toLocaleString("en-us", { month: "short" });
 
-const canShowYear = computed(() => props.disable.toString() !== 'year');
-const canShowMonth = computed(() => props.disable.toString() !== 'month');
+const canShowYear: ComputedRef<boolean> = computed(
+  () => props.disable.toString() !== "year"
+);
+const canShowMonth: ComputedRef<boolean> = computed(
+  () => props.disable.toString() !== "month"
+);
 
-const presentDate = {
-    year: canShowYear.value
-        ? ACTUAL_DATE_YEAR.toString()
-        : '',
-    month: canShowMonth.value
-        ? getMonthShortNameByIndex(ACTUAL_DATE.getMonth())
-        : ''
+const presentDate: ModelValue = {
+  year: canShowYear.value ? ACTUAL_DATE_YEAR.toString() : "",
+  month: canShowMonth.value
+    ? getMonthShortNameByIndex(ACTUAL_DATE.getMonth())
+    : "",
 };
 
 const isPresent = ref(false);
 
-watch(isPresent, () => {
-    if (!isPresent.value) return;
-    data.value = { ...presentDate };
-});
+const handlePresentDate = () => {
+  if (!isPresent.value) return;
+  data.value = { ...presentDate };
+};
 
-watch(data, () => {
-    if (isEqual(data.value, presentDate)) return;
-    isPresent.value = false;
-}, { deep: true })
+watch(isPresent, handlePresentDate);
 
+const handleDate = (): void => {
+  if (isEqual(data.value, presentDate)) return;
+  isPresent.value = false;
+};
 
+watch(data, handlePresentDate, { deep: true });
 </script>
 
 <template>
-    <div class="">
-        <year-input v-model="data.year" v-show="canShowYear" :start-from-end="props.startFromEnd" />
-        <label class="label cursor-pointer" v-show="props.present">
-            <span class="label-text">Present</span>
-            <input type="checkbox" v-model="isPresent" class="checkbox checkbox-primary" />
-        </label>
-        <month-input v-model="data.month" v-show="canShowMonth" />
-    </div>
+  <div class="">
+    <year-input
+      v-model="data.year"
+      v-show="canShowYear"
+      :start-from-end="props.startFromEnd"
+    />
+    <label class="label cursor-pointer" v-show="props.present">
+      <span class="label-text">Present</span>
+      <input
+        type="checkbox"
+        v-model="isPresent"
+        class="checkbox checkbox-primary"
+      />
+    </label>
+    <month-input v-model="data.month" v-show="canShowMonth" />
+  </div>
 </template>
 
 <style scoped></style>
