271afa2994c61d9d7f1764696e93cff36779c29f
simpllleser <pecherytsia.vv@gmail.com>
2023-03-24 01:48:24 +0200

    refactor(year-input): add types

--- src/components/Input/YearInput.vue
+++ src/components/Input/YearInput.vue
@@ -1,17 +1,20 @@
 <script setup lang="ts">
-import { computed, ref } from "vue";
+import { computed, ComputedRef, ref } from "vue";
 import { useVModel } from "@vueuse/core";
-import { range } from 'lodash';
+import { range } from "lodash";
 
 interface Props {
-    modelValue: string | undefined;
-    startFromEnd: boolean,
+  modelValue: string | undefined;
+  startFromEnd: boolean;
 }
 
+interface Emits {
+  (event: "update:modelValue", payload: string): void;
+}
 
 const props = withDefaults(defineProps<Props>(), {
-    startFromEnd: false,
-    disable: '',
+  startFromEnd: false,
+  disable: "",
 });
 
 const ACTUAL_DATE_YEAR = new Date().getFullYear();
@@ -19,63 +22,76 @@ const START_DATE = 1939;
 const QUANTITY_YEARS = 11;
 const DEFAULT_ACTTIVE_PAGE = 1;
 const MAX_ACTTIVE_PAGE = 7;
-const initActivePage = props.startFromEnd ? MAX_ACTTIVE_PAGE : DEFAULT_ACTTIVE_PAGE;
+
+const initActivePage = props.startFromEnd
+  ? MAX_ACTTIVE_PAGE
+  : DEFAULT_ACTTIVE_PAGE;
+
+const emit = defineEmits<Emits>();
+const data = useVModel(props, "modelValue", emit);
+
 const activePage = ref(initActivePage);
 
-
-const emit = defineEmits<{
-    (event: 'update:modelValue', payload: string): void
-}>()
-const data = useVModel(props, 'modelValue', emit)
-
-const setActiveYearFrom = (year: number) => {
-    data.value = year.toString();
+const setActiveYearFrom = (year: number): void => {
+  data.value = year.toString();
 };
 
-const setPage = (page: number) => { activePage.value += page };
+const setPage = (page: number): void => {
+  activePage.value += page;
+};
 
 const isMaxYear = (year: number): Boolean => year === ACTUAL_DATE_YEAR;
 const isNotMaxYear = (year: number): Boolean => year <= ACTUAL_DATE_YEAR;
-const existMaxYear = computed(() => yearsRange.value
-    .find(isMaxYear));
 
-const yearsRange = computed(() => {
-    const fromDate = activePage.value > DEFAULT_ACTTIVE_PAGE
-        ? START_DATE + (activePage.value * QUANTITY_YEARS)
-        : START_DATE;
-    return range(
-        fromDate,
-        fromDate + QUANTITY_YEARS,
-        DEFAULT_ACTTIVE_PAGE);
+const yearsRange: ComputedRef<Array<number>> = computed(() => {
+  const fromDate =
+    activePage.value > DEFAULT_ACTTIVE_PAGE
+      ? START_DATE + activePage.value * QUANTITY_YEARS
+      : START_DATE;
+  return range(fromDate, fromDate + QUANTITY_YEARS, DEFAULT_ACTTIVE_PAGE) || [];
 });
-const actualYearsRange = computed(() => yearsRange.value.filter(isNotMaxYear));
-const isCurrentedDate = (date: string) => date === data.value?.toString(); 
 
+const existMaxYear: ComputedRef<boolean> = computed(
+  () => Boolean(yearsRange.value?.find(isMaxYear)) || false
+);
+const actualYearsRange: ComputedRef<number[]> = computed(() =>
+  yearsRange.value.filter(isNotMaxYear)
+);
+const isCurrentedDate = (date: string): boolean =>
+  date === data.value?.toString();
 </script>
 
 <template>
-        <div class="grid grid-cols-4">
-            <div class="flex">
-                <div class="p-1">
-                    <div class="rotate-90" @click="setPage(-1)" v-show="activePage > 1">
-                        <unicon name="angle-down" class="text-xs" fill="black"></unicon>
-                    </div>
-                </div>
-                <div class="p-1">
-                    <div style="transform: rotate(-90deg);" @click="setPage(1)" v-show="!existMaxYear">
-                        <unicon name="angle-down" class="text-xs" fill="black"></unicon>
-                    </div>
-                </div>
-            </div>
-            <div v-for="year in actualYearsRange" :key="year" @click="setActiveYearFrom(year)"
-                class="p-1 cursor-pointer text-center" :class="{
-                    'bg-primary text-white': isCurrentedDate(year.toString())
-                }">
-                {{ year }}
-            </div>
+  <div class="grid grid-cols-4">
+    <div class="flex">
+      <div class="p-1">
+        <div class="rotate-90" @click="setPage(-1)" v-show="activePage > 1">
+          <unicon name="angle-down" class="text-xs" fill="black"></unicon>
         </div>
+      </div>
+      <div class="p-1">
+        <div
+          style="transform: rotate(-90deg)"
+          @click="setPage(1)"
+          v-show="!existMaxYear"
+        >
+          <unicon name="angle-down" class="text-xs" fill="black"></unicon>
+        </div>
+      </div>
+    </div>
+    <div
+      v-for="year in actualYearsRange"
+      :key="year"
+      @click="setActiveYearFrom(year)"
+      class="p-1 cursor-pointer text-center"
+      :class="{
+        'bg-primary text-white': isCurrentedDate(year.toString()),
+      }"
+    >
+      {{ year }}
+    </div>
+  </div>
 </template>
 
 <style scoped>
-
 </style>
