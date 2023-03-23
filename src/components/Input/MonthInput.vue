b8df410880262d5be416164e0feee5e8f5b833f9
simpllleser <pecherytsia.vv@gmail.com>
2023-03-24 01:42:20 +0200

    refactor<month-input>: add interfaces

--- src/components/Input/MonthInput.vue
+++ src/components/Input/MonthInput.vue
@@ -1,45 +1,56 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { Ref, ref } from "vue";
 import { useVModel } from "@vueuse/core";
 
-const emit = defineEmits<{
-    (event: 'update:modelValue', payload: string): void
-}>()
+interface Emits {
+  (event: "update:modelValue", payload: string): void;
+}
+
+const emit = defineEmits<Emits>();
 
 interface Props {
-    modelValue: string | undefined;
+  modelValue: string | undefined;
 }
 
 const props = withDefaults(defineProps<Props>(), {});
 
-const data = useVModel(props, 'modelValue', emit)
+const data = useVModel(props, "modelValue", emit);
 
-const getMonthShortNameByIndex = (index: number) => new Date(2000, index)
-    .toLocaleString('en-us', { month: 'short' });
+const getMonthShortNameByIndex = (index: number): string =>
+  new Date(2000, index).toLocaleString("en-us", { month: "short" });
 
-const months = new Array(12)
-    ?.fill('')
-    .map((_, index) => getMonthShortNameByIndex(index));
+const months: Array<string> = new Array(12)
+  ?.fill("")
+  .map((_, index) => getMonthShortNameByIndex(index));
 
-const present = ref(false);
-const resetPresent = () => { present.value = false };
+const present: Ref<boolean> = ref(false);
 
-const setActiveMonthFrom = (month: string) => {
-    if (data.value !== month) resetPresent();
-    data.value = month;
+const resetPresent = (): void => {
+  present.value = false;
+};
+
+const setActiveMonthFrom = (month: string): void => {
+  if (data.value !== month) resetPresent();
+  data.value = month;
 };
 </script>
 
 <template>
-        <div class="grid grid-cols-3">
-            <div v-for="month in months" :key="month" @click="setActiveMonthFrom(month)" class="p-1" :class="{
-                'bg-primary text-white': month === data
-            }">
-                <div class="cursor-pointer text-center">
-                    {{ month }}
-                </div>
-            </div>
-        </div>
+  <div class="grid grid-cols-3">
+    <div
+      v-for="month in months"
+      :key="month"
+      @click="setActiveMonthFrom(month)"
+      class="p-1"
+      :class="{
+        'bg-primary text-white': month === data,
+      }"
+    >
+      <div class="cursor-pointer text-center">
+        {{ month }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
