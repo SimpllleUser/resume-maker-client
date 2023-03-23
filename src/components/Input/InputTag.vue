454a86ee0fb9617b5ded4c93e3b1c756476da3c5
simpllleser <pecherytsia.vv@gmail.com>
2023-03-24 01:34:39 +0200

    refactor(input-tag) add interface props

--- src/components/Input/InputTag.vue
+++ src/components/Input/InputTag.vue
@@ -1,24 +1,24 @@
 <script setup lang="ts">
-import { ref } from "vue";
 import contenteditable from "vue-contenteditable";
 import { useVModel } from "@vueuse/core";
 
-// const isEditable = ref(true);
-// const content = ref("Full Name User");
-
-const props = defineProps<{
+interface Props {
   class?: string;
   style?: string;
   modelValue: string;
-}>();
+}
 
 interface Emits {
   (event: "update:modelValue", payload: string): void;
 }
 
+const props = defineProps<Props>();
+
+
 const emit = defineEmits<Emits>();
 
 const model = useVModel(props, "modelValue", emit);
+
 </script>
 <template>
   <div>
