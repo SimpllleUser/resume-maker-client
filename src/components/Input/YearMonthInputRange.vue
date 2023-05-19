b073089d43d26302cb03547f8ebd5cfe4910690b
simpllleser <pecherytsia.vv@gmail.com>
2023-05-20 01:10:43 +0300

    fix(YearMonthInputRange): change start date in date start

--- src/components/Input/YearMonthInputRange.vue
+++ src/components/Input/YearMonthInputRange.vue
@@ -68,7 +68,11 @@ const setPresent = (state: boolean) => { isPresent.value = state; };
         <div class="text-center text-gray-400">
           <b>From</b>
         </div>
-        <year-month-input v-model="data.from" :disable="disable" />
+        <year-month-input 
+         v-model="data.from"
+         :disable="disable"
+         start-from-end
+         />
       </div>
       <div class="p-1">
         <div class="text-center text-gray-400">
